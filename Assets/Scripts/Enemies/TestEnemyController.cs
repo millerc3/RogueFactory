ed01d120b7e665da8f118044aaa1c79430ee00e1
Chris <cmiller9713@yahoo.com>
2023-06-17 12:14:05 -0400

    added state machine code

--- Assets/Scripts/Enemies/TestEnemyController.cs
+++ Assets/Scripts/Enemies/TestEnemyController.cs
@@ -1,23 +1,147 @@
 using EasyCharacterMovement;
+using Mono.CSharp.Linq;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.AI;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class TestEnemyController : AgentCharacter
 {
-    private Transform targetTransform;
+    private StateMachine stateMachine;
+
+    private Transform currentTarget;
+
+    protected override void OnAwake()
+    {
+        base.OnAwake();
+
+        stateMachine = new StateMachine();
+        var player = FindObjectOfType<TPSCombatCharacterController>();
+
+        // Define state machine states
+        var searchState = new SearchForPlayer(this, player);
+        var meleeAttackState = new MeleeAttackPlayer(this, GetNavMeshAgent(), player);
+
+        // Define state machine transitions
+        stateMachine.AddTransition(searchState, 
+                                   meleeAttackState, 
+                                   () => Vector3.Distance(transform.position, player.GetPosition()) < 1f);
+
+        stateMachine.SetState(searchState);
+    }
 
     protected override void OnStart()
     {
         base.OnStart();
 
-        targetTransform = FindObjectOfType<TPSCombatCharacterController>()?.transform;
+
     }
 
     protected override void OnUpdate()
     {
         base.OnUpdate();
 
-        MoveToLocation(targetTransform.position);
+
     }
+
+    private void Attack()
+    {
+        print($"Attacking!");
+    }
+
+
+    #region States
+
+    public class SearchForPlayer : IState
+    {
+        private TestEnemyController enemyController;
+        private TPSCombatCharacterController player;
+
+        public SearchForPlayer(TestEnemyController _enemyController, TPSCombatCharacterController _player)
+        {
+            enemyController = _enemyController;
+            player = _player;
+        }
+
+        void IState.Tick()
+        {
+            enemyController.currentTarget = player.transform;
+        }
+
+        void IState.OnEnter()
+        {
+        }
+
+        void IState.OnExit()
+        {
+        }
+    }
+
+    public class MoveTowardsTarget : IState
+    {
+        private TestEnemyController enemyController;
+
+        public MoveTowardsTarget(TestEnemyController _enemyController)
+        {
+            enemyController = _enemyController;
+        }
+
+        void IState.OnEnter()
+        {
+
+        }
+
+        void IState.OnExit()
+        {
+
+        }
+
+        void IState.Tick()
+        {
+            enemyController.GetNavMeshAgent().SetDestination(enemyController.currentTarget.transform.position);
+        }
+    }
+
+    public class MeleeAttackPlayer : IState
+    {
+        private TestEnemyController enemyController;
+        private NavMeshAgent agent;
+        private TPSCombatCharacterController player;
+
+        private float meleeAttackCooldown = 1.25f;
+        private float meleeTimer = 0f;
+
+        public MeleeAttackPlayer(TestEnemyController _enemyController, NavMeshAgent _agent, TPSCombatCharacterController _player)
+        {
+            enemyController = _enemyController;
+            agent = _agent;
+            player = _player;
+        }
+
+        void IState.Tick()
+        {
+            if (meleeTimer <= 0f)
+            {
+                enemyController.Attack();
+                meleeTimer = meleeAttackCooldown;
+            }
+
+            meleeAttackCooldown -= Time.deltaTime;
+        }
+
+        void IState.OnEnter()
+        {
+            
+        }
+
+        void IState.OnExit()
+        {
+
+        }
+    }
+
+    #endregion
 }
---
+++ Assets/Scripts/StateMachines/StateMachine.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    // Current state
+    private IState currentState;
+
+    // Are of all the transitions that exist in this state machine
+    private Dictionary<Type, List<Transition>> transitions = new Dictionary<Type, List<Transition>>();
+    // Transitions that are currently available to us, given our current state
+    private List<Transition> currentTransitions = new List<Transition>();
+    // Transitions that are ALWAYS available to us, no matter our current state
+    private List<Transition> anyTransitions = null;
+
+    // Nice helper to just initialize an empty list of transitions
+    private static List<Transition> EmptyTransitions = new List<Transition>(0);
+
+
+    public void Tick()
+    {
+        Transition transition = GetTransition();
+        if (transition != null)
+        {
+            SetState(transition.To);
+        }
+
+        currentState?.Tick();
+    }
+
+    public void SetState(IState state)
+    {
+        if (state == currentState) return;
+
+        currentState?.OnExit();
+        currentState = state;
+
+        transitions.TryGetValue(currentState.GetType(), out var currentTransitions);
+        if (currentTransitions == null)
+        {
+            currentTransitions = EmptyTransitions;
+        }
+
+        currentState.OnEnter();
+    }
+
+    public void AddTransition(IState fromState, IState toState, Func<bool> predicate)
+    {
+        // If we don't have the <fromState> stored in our transitions dict, we can't add this new transition to it
+        if (!transitions.TryGetValue(fromState.GetType(), out var fromStateTransitions))
+        {
+            // Make a new list of transitions for us to set as the transitions stemming from <fromState>
+            fromStateTransitions = new List<Transition>();
+            // Add this list to the fromState index of our transitions dict
+            transitions[fromState.GetType()] = fromStateTransitions;
+        }
+
+        // add this transition to the list of transitions stemming from the <fromState>
+        fromStateTransitions.Add(new Transition(toState, predicate));
+    }
+
+    public void AddAnyTransition(IState toState, Func<bool> predicate)
+    {
+        anyTransitions.Add(new Transition(toState, predicate));
+    }
+
+    /// <summary>
+    /// Given a positive Condition, what state will happen next
+    /// </summary>
+    private class Transition
+    {
+        public Func<bool> Condition { get; }
+        public IState To { get; }
+
+        public Transition(IState to, Func<bool> condition)
+        {
+            To = to;
+            Condition = condition;
+        }
+    }
+
+    private Transition GetTransition()
+    {
+        foreach (Transition transition in anyTransitions)
+        {
+            if (transition.Condition())
+            {
+                return transition;
+            }
+        }
+
+        foreach (Transition transition in currentTransitions)
+        {
+            if (transition.Condition())
+            {
+                return transition;
+            }
+        }
+
+        return null;
+    }
+
+}
